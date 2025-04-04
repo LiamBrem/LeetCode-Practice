35150cd0198a7122e33aea6debb1655cc8335420
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2025-04-04 17:11:43 -0400

    Time: 4 ms (9.44%), Space: 18.1 MB (39.3%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -1,17 +1,32 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        seen = {}
-        for pair in nums1:
-            seen[pair[0]] = pair[1]
-
-        for pair in nums2:
-            if pair[0] in seen.keys():
-                seen[pair[0]] += pair[1]
-            else:
-                seen[pair[0]] = pair[1]
-
+        p1 = 0
+        p2 = 0
         result = []
-        for key, value in seen.items():
-            result.append([key, value])
 
-        return sorted(result)
+        while p1 < len(nums1) and p2 < len(nums2):
+            id1, val1 = nums1[p1]
+            id2, val2 = nums2[p2]
+
+            if id1 < id2:
+                result.append([id1, val1])
+                p1 += 1
+            elif id1 > id2:
+                result.append([id2, val2])
+                p2 += 1
+            else:
+                result.append([id1, val1 + val2])
+                p1 += 1
+                p2 += 1
+
+        
+        while p1 < len(nums1):
+            result.append(nums1[p1])
+            p1 += 1
+
+        while p2 < len(nums2):
+            result.append(nums2[p2])
+            p2 += 1
+
+
+        return result
