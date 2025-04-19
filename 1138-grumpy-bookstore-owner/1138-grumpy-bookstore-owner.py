d2fc1b47101d09652a859e775f7ccf14f74609c0
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2025-04-19 18:57:25 -0400

    Time: 23 ms (29.02%), Space: 19.2 MB (92.18%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.py
@@ -1,25 +1,23 @@
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         
-        maxCount = 0
-        maxIndex = 0
-        for i in range(0, len(customers) - minutes + 1):
-            currentCount = 0
-            for j in range(i, i + minutes):
-                if grumpy[j] == 1:
-                    currentCount += customers[j]
-            if currentCount > maxCount:
-                maxCount = currentCount
-                maxIndex = i
+        l = 0
+        window = 0
+        result = 0
+        satisfied = 0
+        maxWindow = 0
+        for r in range(len(customers)):
+            if grumpy[r]:
+                window += customers[r]
+            else:
+                satisfied += customers[r]
+            
+            if (r - l + 1) > minutes:
+                if grumpy[l] == 1:
+                    window -= customers[l]
+                l += 1
 
-        for i in range(maxIndex, maxIndex + minutes):
-            grumpy[i] = 0
+            maxWindow = max(window, maxWindow)
 
-        print(grumpy)
 
-        res = 0
-        for i in range(len(customers)):
-            if grumpy[i] == 0:
-                res += customers[i]
-
-        return res
+        return satisfied + maxWindow
