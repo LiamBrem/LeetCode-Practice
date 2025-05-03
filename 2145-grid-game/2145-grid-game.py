6bd47474ae05ef771cb2c2018f29bb68a44445e2
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2025-05-02 20:17:33 -0400

    Time: 91 ms (59.92%), Space: 28.6 MB (96.52%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -1,6 +1,6 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        minimum = 100000000000000000
+        minimum = float('inf')
 
         r1 = r2 = 0
         for i in range(len(grid[0])):
@@ -8,7 +8,7 @@ class Solution:
         
         for i in range(len(grid[0])):
             r1 -= grid[0][i]
-            minimum = min(minimum, max(r1, r2))
+            minimum = int(min(minimum, max(r1, r2)))
             r2 += grid[1][i]
 
-        return int(minimum)
+        return minimum
