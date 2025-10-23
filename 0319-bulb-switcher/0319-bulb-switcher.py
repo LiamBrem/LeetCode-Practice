9aa8185d0aab1e2977ffbe4dc8226027e4917f1a
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2025-10-23 16:42:59 -0400

    Time: 0 ms (100%), Space: 17.7 MB (48.27%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -0,0 +1,13 @@
+"""
+[1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0]
+[1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+
+3
+4
+"""
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+
+        return floor(sqrt(n))
+        
