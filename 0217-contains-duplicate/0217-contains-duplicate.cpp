75d7df40263c9251ca7a01b54517fadc5a27b247
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2025-10-29 20:09:28 -0400

    Time: 51 ms (54.76%), Space: 90.9 MB (30.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,7 +3,6 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         std::unordered_set<int> hashSet;
 
-
         for (int i = 0; i < nums.size(); i++){
             if (hashSet.count(nums[i]) == 1){
                 return 1; 
