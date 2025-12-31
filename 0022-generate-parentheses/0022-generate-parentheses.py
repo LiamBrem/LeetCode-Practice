223ddfefed5052be9d1919be2f9d4b4b8f2d7d1b
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2025-12-31 16:13:31 -0500

    Time: 3 ms (32.53%), Space: 17.7 MB (85.37%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,29 @@
+"""
+())(
+
+"""
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        maxLen = n * 2
+
+        def dfs(openCount, completedCount, curr):
+            if len(curr) >= maxLen:
+                res.append(curr)
+                return
+
+            if len(curr) == 0:
+                dfs(1, 0, "(")
+
+            if openCount >= n:
+                dfs(openCount, completedCount + 1, curr + ")")
+
+            else:
+                dfs(openCount + 1, completedCount, curr + "(")
+                if openCount > completedCount:
+                    dfs(openCount, completedCount + 1, curr + ")")
+
+        dfs(0, 0, "")
+
+        return res[:len(res) // 2]
