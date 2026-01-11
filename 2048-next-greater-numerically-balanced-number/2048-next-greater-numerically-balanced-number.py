f9712c30f66faf633180d2f4dfbc8ed646a8bc8d
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-01-11 10:39:15 -0500

    Time: 91 ms (75.85%), Space: 22 MB (5.08%) - LeetHub

--- 2048-next-greater-numerically-balanced-number/2048-next-greater-numerically-balanced-number.py
+++ 2048-next-greater-numerically-balanced-number/2048-next-greater-numerically-balanced-number.py
@@ -1,17 +1,58 @@
+from itertools import permutations
 class Solution:
     def nextBeautifulNumber(self, n: int) -> int:
-        def isOccuring(n):
-            s = str(n)
-            c = Counter(s)
-            for key in c.keys():
-                if c[key] != int(key):
-                    return False
+        balancedNums = []
+        seen = set()
+        bases = [
+            {"1":1},
+            {"2":2},
+            {"1":1,"2":2},
+            {"3":3},
+            {"1":1,"3":3},
+            {"4":4},
+            {"1":1,"4":4},
+            {"2":2,"3":3},
+            {"5":5},
+            {"1":1,"2":2,"3":3},
+            {"1":1,"5":5},
+            {"2":2,"4":4},
+            {"6":6},
+            {"1":1,"2":2,"4":4},
+            {"1":1,"6":6},
+            {"2":2,"5":5},
+            {"3":3,"4":4},
+            {"7":7},
+            {"1":1,"2":2,"5":5},
+        ]
 
-            return True
 
-        n += 1
-        while True:
-            if isOccuring(n):
-                return n
-            n += 1
+        def dfs(counts, path):
+            if sum(counts.values()) == 0:
+                balancedNums.append(int(path))
+                return
 
+            for d in list(counts.keys()):
+                if counts[d] > 0:
+                    counts[d] -= 1
+                    dfs(counts, path + d)
+                    counts[d] += 1
+               
+            
+        for base in bases:
+            dfs(base, "")
+
+        balancedNums.sort()
+
+        l, r = 0, len(balancedNums) - 1
+
+        while l < r:
+            mid = (l + r) // 2
+            if balancedNums[mid] > n:
+                r = mid
+
+            elif balancedNums[mid] <= n:
+                l = mid + 1
+
+
+        return balancedNums[l]
+    
