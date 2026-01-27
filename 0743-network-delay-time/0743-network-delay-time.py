c446c09b8acad08c2c6cb0c68c57a41b4bcf5516
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-01-27 10:19:22 -0500

    Time: 343 ms (81.65%), Space: 21.7 MB (5.6%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,30 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        distances = [float('inf')] * n
+        distances[k - 1] = 0
+        adj = defaultdict(list)
+        for u, v, w in times:
+            adj[u].append((v, w))
+
+        print(distances)
+        print(adj)
+
+        q = [(0, k)]
+
+        while q: # while there are still unvisited nodes
+            currDist, currNode = heapq.heappop(q)
+
+            if currDist > distances[currNode - 1]:
+                continue
+
+            for neighbor, weight in adj[currNode]:
+                distance = currDist + weight
+                if distance < distances[neighbor - 1]:
+                    distances[neighbor - 1] = distance
+                    heapq.heappush(q, (distance, neighbor))
+
+        return -1 if float('inf') in distances else max(distances)
+
+
+
+            
