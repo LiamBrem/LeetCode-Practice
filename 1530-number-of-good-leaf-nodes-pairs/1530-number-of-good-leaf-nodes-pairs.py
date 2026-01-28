9c7d67c0cc40e72516e1d27a754c41bdaf2750ce
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-01-28 14:36:02 -0500

    Time: 3283 ms (5.32%), Space: 21.7 MB (5.32%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countPairs(self, root: Optional[TreeNode], distance: int) -> int:
+        adj = defaultdict(list) 
+        leafNodes = set() 
+
+        def iterate(curr):
+            if not curr:
+                return
+
+            if curr.left:
+                adj[curr].append(curr.left)
+                adj[curr.left].append(curr)
+                iterate(curr.left)
+
+            if curr.right:
+                adj[curr].append(curr.right)
+                adj[curr.right].append(curr)
+                iterate(curr.right)
+
+            if not curr.left and not curr.right:
+                leafNodes.add(curr)
+
+            return
+
+        iterate(root)
+
+        res = 0
+        path = set()
+
+        def dfs(curr, currDist):
+            nonlocal res
+            if not curr or currDist > distance:
+                return
+
+            if currDist > 0 and curr in leafNodes and curr not in path:
+                print("dist", currDist, "node", curr.val)
+                res += 1
+
+            path.add(curr)
+
+            for neighbor in adj[curr]:
+                dfs(neighbor, currDist + 1) 
+
+
+        for node in leafNodes:
+            path = set()
+            dfs(node, 0)
+    
+        return res // 2
+
+
+        
