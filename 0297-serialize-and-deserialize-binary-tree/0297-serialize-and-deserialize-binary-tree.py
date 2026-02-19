5cbf744548cf49f147f82c941537dd9f5b3da057
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-02-18 19:52:19 -0500

    Time: 82 ms (80.91%), Space: 23.3 MB (14.81%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -5,18 +5,6 @@
 #         self.left = None
 #         self.right = None
 
-
-"""
-Initially tried serializing like a heap
-where position of child i = i * 2 + 1 and i *2 + 2
-The issue with this is if the tree is sparse -> need a lot of memory for all the Null values
-
-Instead doing a preorder traversal
-
-
-
-""" 
-
 class Codec:
 
     def serialize(self, root):
@@ -25,19 +13,17 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if not root:
-            return ""
-
         res = []
 
-        def dfs(curr):
-            if not curr:
+        def dfs(node):
+            if not node:
                 res.append("N")
                 return
+            
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
 
-            res.append(str(curr.val))
-            dfs(curr.left)
-            dfs(curr.right)
 
         dfs(root)
         return ",".join(res)
@@ -49,27 +35,26 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        arr = data.split(",")[:-1]
-        if len(arr) == 0 or arr[0] == 'N':
+        self.i = 0
+        arr = data.split(",")
+        if arr[0] == "N":
             return None
 
-        self.i = 0
-        root = TreeNode(arr[0])
 
         def dfs():
-            if self.i >= len(arr) or arr[self.i] == "N":
-                self.i += 1
+            val = arr[self.i]
+            self.i += 1
+            if val == "N":
                 return None
 
-            curr = TreeNode(int(arr[self.i]))
-            self.i += 1
+            curr = TreeNode(int(val))
             curr.left = dfs()
             curr.right = dfs()
-
 
             return curr
 
         return dfs()
+
         
 
 # Your Codec object will be instantiated and called as such:
