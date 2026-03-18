cd703f1b97befe7a8fd45070a35d780bf92364ba
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-03-18 17:11:34 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1993-operations-on-tree/1993-operations-on-tree.py
+++ 1993-operations-on-tree/1993-operations-on-tree.py
@@ -1,17 +1,5 @@
-"""
-map {parent: [child, child]}
-map {node: locked/unlocked}
-
-
-"""
 from collections import defaultdict, deque
 class LockingTree:
-    def p(self):
-        print("----------------------------")
-        print("isunlocked:", self.isUnlocked)
-        print("children:", self.children)
-        print("parents:", self.parents)
-
     def __init__(self, parent: List[int]):
         self.isUnlocked = {} # {node: (bool, user)}
         self.children = defaultdict(list) # {parent: [child, child, child]}
@@ -22,30 +10,23 @@ class LockingTree:
             self.parents[i] = p
             self.isUnlocked[i] = [True, None]
 
-        # self.p()
-
     def lock(self, num: int, user: int) -> bool:
         if self.isUnlocked[num][0]:
             self.isUnlocked[num][0] = False
             self.isUnlocked[num][1] = user
-            # self.p()
             return True
         else:
-            # self.p()
             return False
 
     def unlock(self, num: int, user: int) -> bool:
         isUnlocked, username = self.isUnlocked[num]
         if isUnlocked:
-            # self.p()
             return False
         elif user == username or user == "admin":
             self.isUnlocked[num][0] = True
             self.isUnlocked[num][1] = None
-            # self.p()
             return True
         else:
-            # self.p()
             return False
         
 
@@ -54,7 +35,6 @@ class LockingTree:
         curr = num
         while curr != -1:
             if self.isUnlocked[curr][0] == False:
-                # self.p()
                 return False
 
             curr = self.parents[curr]
@@ -72,22 +52,13 @@ class LockingTree:
                 q.append(nxt)
 
         if atLeastOneLocked == False:
-            # self.p()
             return False
         else:
             for numToUnlock in toUnlock:
-                # print(1, self.unlock(numToUnlock, "admin"))
                 self.unlock(numToUnlock, "admin")
 
-            # print(2, self.lock(num, user))
             self.lock(num, user)
-            # self.p()
             return True
-            
-
-
-        
-
 
 # Your LockingTree object will be instantiated and called as such:
 # obj = LockingTree(parent)
