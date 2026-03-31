d4d70858240c9e502c9055e4b567c596aa9bf856
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-03-31 15:16:44 -0400

    Time: 172 ms (33.5%), Space: 74.9 MB (97.42%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.py
@@ -0,0 +1,34 @@
+import heapq
+
+class NumberContainers:
+    def __init__(self):
+        self.idxToNum = {} # {index: number that's there}
+        self.numToIdx = {} # {number: [list of indices]}
+    
+    def change(self, index, number):
+        self.idxToNum[index] = number
+
+        if number not in self.numToIdx or not self.numToIdx[number]:
+            self.numToIdx[number] = [index]
+        else:
+            heapq.heappush(self.numToIdx[number], index)
+   
+    def find(self, number):
+        while number in self.numToIdx and len(self.numToIdx[number]) > 0:
+            idx = self.numToIdx[number][0]
+            if idx in self.idxToNum and self.idxToNum[idx] == number:
+                return idx
+
+            heapq.heappop(self.numToIdx[number])
+
+        return -1
+
+
+
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
