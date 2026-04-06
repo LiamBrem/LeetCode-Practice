fd47d1ee03b2d2f3f5a717f78ab44b7d517369a5
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2026-04-06 16:25:25 -0400

    Time: 4 ms (38.84%), Space: 19.9 MB (23.52%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        @lru_cache(None)
+        def dfs(i, j):
+            if i == j:
+                return nums[i]
+
+            return max(nums[i] - dfs(i + 1, j), nums[j] - dfs(i, j  -1))
+
+        return dfs(0, len(nums) - 1) >= 0
+
+            
+
+            
